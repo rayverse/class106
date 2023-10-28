499f5fd3c948c65f99bef86ee1bc66af12f9975b
rayverse <88581751+rayverse@users.noreply.github.com>
2023-10-28 19:45:20 +0530

    Add files via upload

---
+++ PRO-C106-Teacher-Boilerplate-main/faces.py
@@ -0,0 +1,12 @@
+import cv2
+
+img= cv2.imread("4f.jpg")
+gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faceCascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faces=faceCascade.detectMultiScale(gray, 1.5, 3)
+
+for(x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y), (x+w, y+h), (255,0,0), 2)
+    
+cv2.imshow("img", img)
+cv2.waitKey(0)
